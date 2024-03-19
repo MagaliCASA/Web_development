6809ec3123b75ee56136b6ad3a9f915027e8d87a
MagaliCASA <Magali.casamayou@gmail.com>
2024-03-19 11:54:24 +0100

    cours1

--- TP1/frontend/src/pages/Home/Home.jsx
+++ TP1/frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,36 @@
 import logo from './logo.svg';
 import './Home.css';
+import React, { useEffect, useState } from 'react';
 
 function Home() {
+  const [movieName, setmovieName] = useState("");
+
+  useEffect(()=>{
+
+  }, [movieName])
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <label>
+        Recherche de films : 
+          <input type="text" Name="film" value={movieName} onChange={(event) =>
+          setmovieName(event.target.value)}/>
+        </label>
+        <input type="submit" value="Submit" />
+
+        <h3>Résultats de la recherche</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {movieName}
         </p>
+        <h1>Recommendations de films</h1>
+        <p>
+          1. Insaisissables
+        </p>
+        <p>
+          2. Indiana Jones
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
