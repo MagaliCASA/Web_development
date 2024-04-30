d8bf2af10b2787ef8882e109100e8f55180d6dfb
Frysperz <cybalgauthier@gmail.com>
2024-04-30 11:20:33 +0200

    changement backend

--- TP1/backend/routes/movies.js
+++ TP1/backend/routes/movies.js
@@ -42,6 +42,41 @@ router.post('/new', (req, res) => {
       })
 });
 
+router.post('/newmovie', (req, res) => {
+  const movieRepository = appDataSource.getRepository(Movie);
+  const id = req.body.id;
+  
+  axios
+      .get(`https://api.themoviedb.org/3/movie/${id}?api_key=57359ff087905e870d40ba4880a1dce0`)
+      .then((response) => {
+        const newMovie = movieRepository.create({
+          genre: response.genres,
+          date: response.release_date,
+          title: response.original_title,
+          resume: response.overview,
+          affiche: response.poster_path,
+        });
+      movieRepository
+        .insert(newMovie)
+        .then( () => {
+          res.json({
+            message:`Le film ${newMovie.name} a bien été ajouté`
+          })
+          // console.log('le film a bien été ajouté')
+          })
+        .catch(function (error) {
+          console.error(error);
+          if (error.code === '23505') {
+            res.status(400).json({
+              message: `Le film ${newMovie.name} existe déjà`,
+            });
+          } else {
+            res.status(500).json({ message: 'Erreur dans l\'ajout du film'});
+          }
+        })
+    })
+});
+
 router.delete('/delete', (req,res) => {
   const movieRepository = appDataSource.getRepository(Movie);
 
