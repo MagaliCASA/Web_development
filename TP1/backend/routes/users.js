f8904674add674b79f02ecb4da9c8faa2393be16
Laura MONTAGNIER <laura.montagnier@ensta-paris.fr>
2024-05-05 17:21:33 +0200

    On peut se connecter !

--- TP1/backend/routes/users.js
+++ TP1/backend/routes/users.js
@@ -4,6 +4,33 @@ import User from '../entities/user.js';
 
 const router = express.Router();
 
+router.post('/login', function (req, res) {
+  const { email, password } = req.body;
+
+  appDataSource
+    .getRepository(User)
+    .findOne({ where: { email: email } }) // Ajoutez les conditions de sélection ici
+    .then(function (user) {
+      if (!user) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
+
+      if (user.password !== password) {
+        res.status(401).json({ message: 'Invalid password' });
+        return;
+      }
+
+      // Authentification réussie
+      res.status(200).json({ message: 'Login successful', user: user });
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error while logging in' });
+    });
+});
+
+
 router.get('/', function (req, res) {
   appDataSource
     .getRepository(User)
@@ -17,7 +44,7 @@ router.post('/new', function (req, res) {
   const userRepository = appDataSource.getRepository(User);
   const newUser = userRepository.create({
     email: req.body.email,
-    name: req.body.name,
+    name: req.body.name, // Ajoute le nom de l'utilisateur
     password: req.body.password,
   });
 
@@ -37,6 +64,7 @@ router.post('/new', function (req, res) {
       }
     });
 });
+
 
 router.delete('/:userId', function (req, res) {
   appDataSource
