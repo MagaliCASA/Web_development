68cdeea84aeb5d0cc5d3b36dbdf60c63a8179cbd
Laura MONTAGNIER <laura.montagnier@ensta-paris.fr>
2024-05-05 15:45:33 +0200

    blabla

--- TP1/frontend/src/pages/Users/Users.jsx
+++ TP1/frontend/src/pages/Users/Users.jsx
@@ -1,13 +1,21 @@
+// Users.js
+import React, { useState } from 'react';
 import './Users.css';
 import AddUserForm from '../../components/AddUserForm/AddUserForm';
 import UsersTable from '../../components/UsersTable/UsersTable';
 
 function Users() {
+  const [users, setUsers] = useState([]);
+
+  const handleAddUser = (newUser) => {
+    setUsers([...users, newUser]);
+  };
+
   return (
     <div className="Users-container">
-      <h1>This page displays the users</h1>
-      <AddUserForm />
-      <UsersTable />
+      <h1>Cette page permet de se connecter.</h1>
+      <AddUserForm onAddUser={handleAddUser} />
+      <UsersTable users={users} />
     </div>
   );
 }
