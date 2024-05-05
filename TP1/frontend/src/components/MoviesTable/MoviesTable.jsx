893045d80bb3353d6fcdd4745f68c894b6aad9dc
GeraltofTrivial <cherel@CHRL-VCT>
2024-05-05 15:51:54 +0200

    debut d'ajout de recherche avec tri

--- TP1/frontend/src/components/MoviesTable/MoviesTable.jsx
+++ TP1/frontend/src/components/MoviesTable/MoviesTable.jsx
@@ -6,7 +6,7 @@ import './MoviesTable.css';
 
 const apiKey = 'a0a7e40dc8162ed7e37aa2fc97db5654';
 const baseUrl = ' https://api.themoviedb.org/3/';
-
+let sort = "primary_release_date.desc";
 
 
 
@@ -18,25 +18,29 @@ function MoviesTable(props) {
   async function getPopularMovies(basepage) {
     try {
       var demande = "movie/popular";
-      if (props.search == "") {
-        demande = "movie/popular";
-      } else {
+      if (props.search != "") {
         demande = "search/movie";
-        //const response = await axios.get(`${baseUrl}search/movie?api_key=${apiKey}&query=${props.search}&page=${props.page}`);
       }
+      if (props.sort_type != "Pas de tri")
+      {
+        demande = "discover/movie";
+        if (props.sort_type == "film recent"){sort = "primary_release_date.desc";}
+        if (props.sort_type == "film ancien"){sort = "primary_release_date.asc";}
+      } 
+      
       //&page=${props.page}
-      const response = await axios.get(`${baseUrl}${demande}?api_key=${apiKey}&page=${basepage}&query=${props.search}`);
+      const response = await axios.get(`${baseUrl}${demande}?api_key=${apiKey}&page=${basepage}&query=${props.search}&sort_by=${sort}`);
       //console.log(response.data.results);
       const movies = response.data.results.map(movie => {
         if(movie.vote_average>=props.note_min){
           return{
             title: movie.title,
             original_title: movie.title,
-          release_date: movie.release_date,
-          imgurl: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
-          image: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
-          id: movie.id, // on prends l'id pour avoir identificateur unique
-          review:movie.vote_average,
+            release_date: movie.release_date,
+            imgurl: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+            image: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+            id: movie.id, // on prends l'id pour avoir identificateur unique
+            review:movie.vote_average,
           };
         }return null;
         
