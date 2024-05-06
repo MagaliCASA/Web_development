d23546f2f76a4662d03d93a3a02313910d0966d2
Laura MONTAGNIER <laura.montagnier@ensta-paris.fr>
2024-05-06 14:36:17 +0200

    Verify

---
+++ TP1/frontend/src/components/ConnectUserForm/VerifyToken.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+
+function useTokenVerification() {
+   const [loggedIn, setLoggedIn] = useState(false);
+   const [name, setName] = useState("");
+
+   useEffect(() => {
+       const user = JSON.parse(localStorage.getItem(""));
+
+       if (!user || !user.token) {
+           setLoggedIn(false);
+           return;
+       }
+
+       fetch("http://localhost:8080/api/auth/verify", {
+           method: "POST",
+           headers: {
+               'jwt-token': user.token
+           }
+       })
+           .then(r => r.json())
+           .then(r => {
+               if ('success' === r.message) {
+                   setLoggedIn(true);
+                   setName(user.name);
+                
+
+               } else {
+                   setLoggedIn(false);
+                   setName("");
+               }
+           })
+           .catch(error => {
+               console.error('Error verifying token:', error);
+               setLoggedIn(false);
+           });
+   }, []);
+
+   return { loggedIn, name };
+}
+
+export default useTokenVerification;
