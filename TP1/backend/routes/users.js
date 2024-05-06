a047e65afd6d7befaa41393d494f00ce71c36c49
Laura MONTAGNIER <laura.montagnier@ensta-paris.fr>
2024-05-06 14:39:28 +0200

    users.js ajouts

--- TP1/backend/routes/users.js
+++ TP1/backend/routes/users.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { appDataSource } from '../datasource.js';
 import User from '../entities/user.js';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -22,7 +23,11 @@ router.post('/login', function (req, res) {
       }
 
       // Authentification réussie
-      res.status(200).json({ message: 'Login successful', user: user });
+      const token = jwt.sign(email, "abcd");
+      console.log(token);
+      res.status(200).json({ message: 'Login successful', token, name: user.name });
+
+
     })
     .catch(function (error) {
       console.error(error);
@@ -77,5 +82,23 @@ router.delete('/:userId', function (req, res) {
       res.status(500).json({ message: 'Error while deleting the user' });
     });
 });
+
+// The verify endpoint that checks if a given JWT token is valid
+router.post('/verify', (req, res) => {
+  const tokenHeaderKey = 'jwt-token'
+  const authToken = req.headers[tokenHeaderKey]
+  try {
+    const verified = jwt.verify(authToken, jwtSecretKey)
+    if (verified) {
+      return res.status(200).json({ status: 'logged in', message: 'success' })
+    } else {
+      // Access Denied
+      return res.status(401).json({ status: 'invalid auth', message: 'error' })
+    }
+  } catch (error) {
+    // Access Denied
+    return res.status(401).json({ status: 'invalid auth', message: 'error' })
+  }
+})
 
 export default router;
