9268e672a771b5ede640ca164fa80801e33afde7
MagaliCASA <magali.casamayou@gmail.com>
2024-05-06 08:45:18 +0200

    à propos

--- TP1/frontend/src/pages/About/About.jsx
+++ TP1/frontend/src/pages/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../About.css'; // Assurez-vous d'avoir un fichier styles.css dans le même répertoire
+import './About.css'; // Assurez-vous d'avoir un fichier styles.css dans le même répertoire
 
 function About() {
   return (
@@ -7,7 +7,7 @@ function About() {
       <h1>À Propos</h1>
       <p>Site créé par Magali CASAMAYOU, Laura MONTAGNIER, Cyprien GAUTHIER et Vincent CHEREL.</p>
       <p>Projet réalisé pendant le cours IN211 à l'ENSTA Paris en 2024.</p>
-      <p>Ce site présente une sorte d'Allo Ciné simplifié sur le thème des poissons : il recense quelques films ayant ce thème et les détaille si l'on clique dessus.</p>
+      <p>Ce site présente des recommendations de films sur le thème des poissons : il recense quelques films ayant ce thème et les détaille si l'on clique dessus. Les fonctionnalités de tri et de recherche ont été implémentées ainsi que celles de connexion qui permettent de donner un avis et de mettre une note à un film.</p>
     </div>
   );
 }
